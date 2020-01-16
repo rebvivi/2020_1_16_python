22b4f9943b668668a80c5dbdc8f4823bf07d31da
rebvivi <chengvivian1225@gmail.com>
2020-01-16 19:42:29 +0800

    implement ball and beam in python

---
+++ ballbeam_gym/__init__.py
@@ -0,0 +1,31 @@
+from gym.envs.registration import register
+
+register(
+    id='BallBeamBalance-v0',
+    entry_point='ballbeam_gym.envs:BallBeamBalanceEnv',
+)
+
+register(
+    id='VisualBallBeamBalance-v0',
+    entry_point='ballbeam_gym.envs:VisualBallBeamBalanceEnv',
+)
+
+register(
+    id='BallBeamSetpoint-v0',
+    entry_point='ballbeam_gym.envs:BallBeamSetpointEnv',
+)
+
+register(
+    id='VisualBallBeamSetpoint-v0',
+    entry_point='ballbeam_gym.envs:VisualBallBeamSetpointEnv',
+)
+
+register(
+    id='BallBeamThrow-v0',
+    entry_point='ballbeam_gym.envs:BallBeamThrowEnv',
+)
+
+register(
+    id='VisualBallBeamThrow-v0',
+    entry_point='ballbeam_gym.envs:VisualBallBeamThrowEnv',
+)
